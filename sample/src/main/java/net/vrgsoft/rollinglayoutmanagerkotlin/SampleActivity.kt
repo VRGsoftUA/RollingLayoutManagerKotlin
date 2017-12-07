933624b5785e58e0a95ba02f297b5328246897cb
Pavlo Nikitin <pawlo2102@gmail.com>
2017-12-07 17:02:10 +0200

    Init

--- sample/src/main/java/net/vrgsoft/rollinglayoutmanagerkotlin/SampleActivity.kt
+++ sample/src/main/java/net/vrgsoft/rollinglayoutmanagerkotlin/SampleActivity.kt
@@ -5,24 +5,18 @@ import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.RecyclerView
 import java.util.*
 
-
-
-
 class SampleActivity : AppCompatActivity() {
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sample)
 
         val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
-
-
         val rollingLayoutManager = RollingLayoutManager(this)
-        val simpleAdapter = SimpleAdapter(createTestData(20))
+        recyclerView.layoutManager = rollingLayoutManager
 
-        recyclerView.setLayoutManager(rollingLayoutManager)
-        recyclerView.setAdapter(simpleAdapter)
+        val simpleAdapter = SimpleAdapter(createTestData(20))
+        recyclerView.adapter = simpleAdapter
     }
 
     private fun createTestData(count: Int): List<String> {
